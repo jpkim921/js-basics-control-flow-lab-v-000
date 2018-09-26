aefe9ae14fdcc76e38c6b5a55f656eb9c4be354b
Jayon Kim <jpkim921@gmail.com>
2018-09-26 22:06:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function scuberGreetingForFeet (ride){
 
   let cost;
   if (ride <= 400) {
-
+    cost = 'This one is on me!'
   } else if (ride > 2000) {
-
+    cost = 'I will gladly take your thirty bucks.'
   } else if (ride > 2500) {
-
+    cost = 'No can do.'
   }
   return cost
 }
