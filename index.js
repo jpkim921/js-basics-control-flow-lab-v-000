c46a75571f1385cb9f3b2e65a5b67348a3facda9
Jayon Kim <jpkim921@gmail.com>
2018-09-26 22:03:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Write your code in this file!
 
 scuberGreetingForFeet(ride){
-  if (ride <= 400)
+  let cost;
+  if (ride <= 400) {
+
+  } else if (ride > 2000) {
+
+  } else if (ride > 2500) {
+
+  }
+  return cost
 }
